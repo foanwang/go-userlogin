9afd26a7bd2e04d8a49cd1ea1e96f0475b53a4de
foanwang <foanwang@Mac-Pro.local>
2017-07-28 02:38:33 +0800

    add model testing user

--- src/main/main.go
+++ src/main/main.go
@@ -9,18 +9,9 @@ import (
 
 func main(){
 	controller := httprouter.New()
-//
-//	controller.GET("/", R.Home)
-//	controller.GET("/welcome", R.Welcome)
-//	controller.GET("/error", R.Error)
-//	controller.GET("/register", R.Register)
-//	controller.GET("/login", R.Login)
-//	controller.GET("/profile/:user", R.Profile)
-//	controller.GET("/profile", R.ProfileParamless)
-//	controller.GET("/logout", R.Logout)
-//
 	controller.POST("/user/register", C.UserRegister)
 	controller.POST("/user/login", C.UserLogin)
+	
 	fmt.Println("login system start...")
 	server := negroni.Classic()
 	server.UseHandler(controller)
